c0ffc98525e577cdb9eb88ddaa4537dc1b782991
svetik <ternasvet@gmail.com>
2020-04-05 17:10:12 +0300

    my init

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+// main.js это основная логика админки
+// по клику
+
 const mainContent = document.querySelector('#main-content');
 const title = document.querySelector('#title');
 
@@ -10,53 +13,4 @@ document.querySelectorAll('#main-nav a').forEach(function (element) {
 
 const tableView = new TableView([], mainContent);
 
-const renderPage = function (page, id) {
-    mainContent.innerHTML = '';
-    switch (page) {
-        case 'categories':
-            title.innerHTML = 'Категории';
-            fetchData('categories')
-                .then(function (data) {
-                    tableView.arr = data.map(function (category) {
-                        console.log(category);
-                        let subCategory = document.createElement('a');
-                        subCategory.innerHTML = 'add';
-                        subCategory.href = '#subcategories';
-                        subCategory.addEventListener('click', function () {
-                            renderPage(this.hash.substr(1), category.id);
-                        });
-                        if (category['subcategories'].length) {
-                            category['subcategories'] = subCategory;
-                        }
 
-                        return category;
-                    });
-                    tableView.render();
-                });
-
-            break;
-        case 'subcategories':
-            title.innerHTML = 'Подкатегории';
-            fetchData('subcategories/category/' + id)
-                .then(function (data) {
-                    //перебор по элементам и запись нового значения
-                    tableView.arr = data.map(function (category) {
-                        delete category['category'];
-                        return category;
-                    });
-                    tableView.render();
-                });
-            break;
-        case 'posts':
-            title.innerHTML = 'Посты';
-            fetchData('posts')
-                .then(function (data) {
-                    tableView.arr = data.data;
-                    tableView.render();
-                });
-            break;
-
-        default:
-            mainContent.innerHTML = 'Страница не найдена';
-    }
-};
---
+++ src/js/routes.js
@@ -0,0 +1,50 @@
+const renderPage = function (page, id) {
+    mainContent.innerHTML = '';
+    switch (page) {
+        case 'categories':
+            title.innerHTML = 'Категории';
+            fetchData('categories')
+                .then(function (data) {
+                    tableView.arr = data.map(function (category) {
+                        console.log(category);
+                        let subCategory = document.createElement('a');
+                        subCategory.innerHTML = 'add';
+                        subCategory.href = '#subcategories';
+                        subCategory.addEventListener('click', function () {
+                            renderPage(this.hash.substr(1), category.id);
+                        });
+                        if (category['subcategories'].length) {
+                            category['subcategories'] = subCategory;
+                        }
+
+                        return category;
+                    });
+                    tableView.render();
+                });
+
+            break;
+        case 'subcategories':
+            title.innerHTML = 'Подкатегории';
+            fetchData('subcategories/category/' + id)
+                .then(function (data) {
+                    //перебор по элементам и запись нового значения
+                    tableView.arr = data.map(function (category) {
+                        delete category['category'];
+                        return category;
+                    });
+                    tableView.render();
+                });
+            break;
+        case 'posts':
+            title.innerHTML = 'Посты';
+            fetchData('posts')
+                .then(function (data) {
+                    tableView.arr = data.data;
+                    tableView.render();
+                });
+            break;
+
+        default:
+            mainContent.innerHTML = 'Страница не найдена';
+    }
+};
