9dd4b6af92760de385792508bb8eaabeb49e0996
svetik <ternasvet@gmail.com>
2020-04-07 18:08:09 +0300

    добавила ссылку добавить

---
+++ src/js/authorization.js
@@ -0,0 +1,24 @@
+// const data = {
+//     "email": "test@gmail.com",
+//     "password": "test"
+// }
+// const url = 'http://kolbasa.qbex.io/api/login';
+//
+// function login(url) {
+//     fetch(url, {
+//         method: 'post',
+//         headers: {
+//             "content-type": ["application/json",]
+//         },
+//         body: 'foo=bar&lorem=ipsum'
+//     })
+//         //.then(json)
+//         .then(function (data) {
+//             console.log('Request succeeded with JSON response', data);
+//         })
+//         .catch(function (error) {
+//             console.log('Request failed', error);
+//         });
+// }
+//
+// // login();
--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
 // перменные, которые необходимы для работы сайта
 const API_URL = 'http://kolbasa.qbex.io/api/';
+
